9e60e059e5e52c0a11132fea332bf3980d5b6d16
Bob Aman <bobaman@google.com>
2011-01-15 01:25:58 +0000

    Totally hacky first-draft implementation that mostly works.
    git-svn-id: https://ruby-webfinger.googlecode.com/svn/trunk@4 381cc753-4b0e-690a-75b3-3fd680438bb5

--- lib/webfinger.rb
+++ lib/webfinger.rb
@@ -13,3 +13,51 @@
 #    limitations under the License.
 
 require 'webfinger/version'
+require 'addressable/uri'
+require 'addressable/template'
+require 'xrd'
+require 'openssl'
+
+module WebFinger
+  def self.normalize_acct_uri(acct_uri)
+    acct_uri = Addressable::URI.parse(acct_uri).omit(:query, :fragment)
+    acct_uri.scheme = 'acct'
+    return acct_uri.normalize
+  end
+
+  def self.fetch_hostmeta(hostname)
+    # TODO(sporkmonger) Fix this, quick and dirty right now.
+    # TODO(sporkmonger) Implement simple LRDD caching.
+    hostmeta_uri = Addressable::Template.new(
+      'https://{hostname}/.well-known/host-meta'
+    ).expand('hostname' => hostname)
+    begin
+      xrd = XRD.fetch_and_parse(hostmeta_uri)
+    rescue OpenSSL::SSL::SSLError => e
+      # Needs to handle 404 case
+      warn(e)
+      if hostmeta_uri.scheme == 'https'
+        hostmeta_uri.scheme = 'http'
+        retry
+      else
+        raise e
+      end
+    end
+  end
+
+  def self.lrdd_template(xrd)
+    return Addressable::Template.new(xrd.links(:rel => 'lrdd')[0].template)
+  end
+
+  def self.lookup(email)
+    email = self.normalize_acct_uri(email)
+    # TODO(sporkmonger)
+    # Lots of testing to make sure everything we do here to split out the
+    # hostname is sane.
+    hostname = email.path.split('@')[-1]
+    hostmeta_xrd = self.fetch_hostmeta(hostname)
+    template = self.lrdd_template(hostmeta_xrd)
+    webfinger_uri = template.expand('uri' => email)
+    return XRD.fetch_and_parse(webfinger_uri)
+  end
+end
